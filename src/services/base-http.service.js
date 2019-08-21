3eedf4ae9705867b76a224e8aa4faacde5075d9d
Joe Previte <joe@echobind.com>
2019-08-20 17:27:02 -0700

    feat: update base url for api

--- src/services/base-http.service.js
+++ src/services/base-http.service.js
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default class BaseHttpService {
-  BASE_URL = 'http://localhost:3000';
+  BASE_URL =
+    "http://jsjoeio-nestjs-task-management-prod-2.us-east-1.elasticbeanstalk.com";
   _accessToken = null;
 
   constructor(routerStore) {
@@ -10,26 +11,30 @@ export default class BaseHttpService {
 
   async get(endpoint, options = {}) {
     Object.assign(options, this._getCommonOptions());
-    return axios.get(`${this.BASE_URL}/${endpoint}`, options)
+    return axios
+      .get(`${this.BASE_URL}/${endpoint}`, options)
       .catch(error => this._handleHttpError(error));
   }
 
   async post(endpoint, data = {}, options = {}) {
     Object.assign(options, this._getCommonOptions());
-    return axios.post(`${this.BASE_URL}/${endpoint}`, data, options)
-      .catch(error => this._handleHttpError(error));  
+    return axios
+      .post(`${this.BASE_URL}/${endpoint}`, data, options)
+      .catch(error => this._handleHttpError(error));
   }
 
   async delete(endpoint, options = {}) {
     Object.assign(options, this._getCommonOptions());
-    return axios.delete(`${this.BASE_URL}/${endpoint}`, options)
-      .catch(error => this._handleHttpError(error));     
+    return axios
+      .delete(`${this.BASE_URL}/${endpoint}`, options)
+      .catch(error => this._handleHttpError(error));
   }
 
   async patch(endpoint, data = {}, options = {}) {
     Object.assign(options, this._getCommonOptions());
-    return axios.patch(`${this.BASE_URL}/${endpoint}`, data, options)
-      .catch(error => this._handleHttpError(error));   
+    return axios
+      .patch(`${this.BASE_URL}/${endpoint}`, data, options)
+      .catch(error => this._handleHttpError(error));
   }
 
   _handleHttpError(error) {
@@ -43,7 +48,7 @@ export default class BaseHttpService {
   }
 
   _handle401() {
-    this.routerStore.push('/signin');
+    this.routerStore.push("/signin");
   }
 
   _getCommonOptions() {
@@ -51,8 +56,8 @@ export default class BaseHttpService {
 
     return {
       headers: {
-        Authorization: `Bearer ${token}`,
-      },
+        Authorization: `Bearer ${token}`
+      }
     };
   }
 
@@ -62,16 +67,16 @@ export default class BaseHttpService {
 
   saveToken(accessToken) {
     this._accessToken = accessToken;
-    return localStorage.setItem('accessToken', accessToken);
+    return localStorage.setItem("accessToken", accessToken);
   }
 
   loadToken() {
-    const token = localStorage.getItem('accessToken');
+    const token = localStorage.getItem("accessToken");
     this._accessToken = token;
     return token;
   }
 
   removeToken() {
-    localStorage.removeItem('accessToken');
+    localStorage.removeItem("accessToken");
   }
 }
